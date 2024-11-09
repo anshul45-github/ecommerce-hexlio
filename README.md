df14af4df72074dd5cf2e20d1d8369af2b4b69f8
Anshul <anshulmendiratta10d@gmail.com>
2024-11-09 14:52:21 +0530

    added authentication

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Hexlio is a modern eCommerce application in building to provide a seamless shopping experience.
 
 ## Tech stack
-Next.js, Tailwind CSS, TypeScript, MongoDB, Clerk for authentication
+Next.js, Tailwind CSS, TypeScript, MongoDB, Clerk for authentication, Shadcn.ui
--- ecommerce-app/app/(root)/page.tsx
+++ ecommerce-app/app/(root)/page.tsx
@@ -1,9 +1,24 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  // const onSubmit = async values
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      my-app
+    // set a padding
+    <div className="font-[family-name:var(--font-geist-sans)] p-4">
+      <SignedIn>
+        <UserButton afterSwitchSessionUrl="/" />
+      </SignedIn>
+      <SignedOut>
+        <Button onClick={() => router.push("/sign-in")}>Sign-in</Button>
+      </SignedOut>
     </div>
   );
 }
